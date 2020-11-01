08944876513c71bd83092412216f23b8c542e1c7
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-11-01 17:29:11 +0900

    edit

---
+++ week4/4_2.R
@@ -0,0 +1,24 @@
+library(MASS)
+library(class)
+data(iris3)
+train<-rbind(iris3[1:25,,2],iris3[1:25,,3])
+train <- scale(train)
+test<-rbind(iris3[26:50,,2],iris3[26:50,,3])
+test <- test
+cl<-factor(c(rep("c",25),rep("v",25)))  
+#訓練データの正しいクラスラベルを順にベクトルデータとして与え、
+#クラスデータとして利用できるようにする。
+
+knn3<-knn(train,test,cl,k=3,prob=TRUE) 
+# 各引数の意味を、helpで調べること。
+
+table(cl,knn3) 
+#テストデータの混同行列を作成する。cl にはテストデータの正解を
+#factor を用いて作成する。この例では、テストデータの数が学習データの
+#数と同じなので、同じcl を流用している。
+
+
+(sum(cl!=knn3))/50 
+# cl!=knn3 でテストデータの正解と計算結果の比較を行い、
+#異なっているところにTRUE が、合っているところにFALSE がたつ。
+#sum(cl!=knn3) は異なっている数を返す。
