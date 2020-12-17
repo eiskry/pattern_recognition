cd0079e1aa7991002c7c0fe6cc01a83f079bee23
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-12-17 11:43:24 +0900

    em

---
+++ week8/em.R
@@ -0,0 +1,13 @@
+library(mclust)
+data(iris)
+petal <- subset(iris,select=c(Petal.Length, Petal.Width))
+petal
+petal.labels <- c(rep(1,50),rep(2,50),rep(3,50))
+petal.labels
+
+irEM <- Mclust(iris[,3:4])
+irEM
+plot(irEM)
+names(irEM)
+irEM$classification
+table(petal.labels, irEM$classification)
